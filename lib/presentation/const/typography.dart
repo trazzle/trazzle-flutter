3531c023baabf940ce1eab729abf12f3ad185c8a
hyunho4532 <kyounguk192@gmail.com>
2025-11-07 22:36:58 +0900

    Feat. Typography 폰트 설정 구현 중

---
+++ lib/presentation/const/typography.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class FontStyle {
+  static const semiBold = FontWeight.w600;
+}
+
+class Typography {
+  static final TextStyle h28 = TextStyle(
+    fontSize: 28,
+    fontWeight: FontStyle.semiBold,
+    height: 24,
+    letterSpacing: 0,
+    color: Colors.black
+  );
+}
