44198256144b9bac4f99098527a7297816cb9677
hyunho4532 <kyounguk192@gmail.com>
2025-11-07 23:08:19 +0900

    Feat. PrimaryColors, SubColors 색상 설정 구현 완료

--- lib/presentation/const/colors.dart
+++ lib/presentation/const/colors.dart
@@ -1,3 +1,27 @@
+import 'dart:ui';
+
 class PrimaryColors {
-  
+  static final p50 = Color(0xffE5F5E8);
+  static final p100 = Color(0xffC1E6C6);
+  static final p200 = Color(0xff99D6A2);
+  static final p300 = Color(0xff6DC67C);
+  static final p400 = Color(0xff49BA60);
+  static final p500 = Color(0xff19AE43);
+  static final p600 = Color(0xff0A9F3A);
+  static final p700 = Color(0xff008D2E);
+  static final p800 = Color(0xff007C23);
+  static final p900 = Color(0xff005D10);
+}
+
+class SubColors {
+  static final sy50 = Color(0xffFFF7AE);
+  static final sy100 = Color(0xffFFF285);
+  static final sy200 = Color(0xffFFEE5B);
+  static final sy300 = Color(0xffFFEA2D);
+  static final sy400 = Color(0xffFFDB1E);
+  static final sy500 = Color(0xffFFC700);
+  static final sy600 = Color(0xffFFB800);
+  static final sy700 = Color(0xffFF9F2F);
+  static final sy800 = Color(0xffFA922C);
+  static final sy900 = Color(0xffF38329);
 }
