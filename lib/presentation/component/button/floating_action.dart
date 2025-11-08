5379a3e291d058c74679c073a3fc7a6503638589
hyunho4532 <kyounguk192@gmail.com>
2025-11-08 22:30:12 +0900

    Feat. FloatingActionButton UI 구현 완료

--- lib/presentation/component/button/floating_action.dart
+++ lib/presentation/component/button/floating_action.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:trazzle/presentation/const/colors.dart';
 
 class FloatingAction extends StatelessWidget {
   const FloatingAction({super.key});
@@ -7,7 +9,13 @@ class FloatingAction extends StatelessWidget {
   Widget build(BuildContext context) {
     return FloatingActionButton(
       onPressed: null,
-
+      backgroundColor: PrimaryColors.p500,
+      shape: CircleBorder(),
+      child: SvgPicture.asset(
+        'assets/add_icon.svg',
+        width: 24,
+        height: 24,
+      ),
     );
   }
 }
