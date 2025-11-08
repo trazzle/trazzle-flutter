679ed3011e99a10e61024348e2b722f6382e910e
hyunho4532 <kyounguk192@gmail.com>
2025-11-08 17:30:09 +0900

    Feat. 스플래시 화면 구현 중 (splash_screen ui 구현)

---
+++ lib/presentation/screens/splash_screen.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(const Duration(seconds: 2), () {
+      Navigator.pushNamed(context, '/home');
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: SvgPicture.asset(
+          'assets/splash_logo.svg',
+          width: 150,
+        ),
+      ),
+    );
+  }
+}
